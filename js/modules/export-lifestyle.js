9d7a4a9163f838d9387dde214d19d171106eb9c3
arnoldabraham <info@arnoldcode.com>
2023-01-12 20:45:56 +0100

    feat(export): add exporting/downloading function and demonstrate the final format we must reach to export into csv

--- js/modules/export-lifestyle.js
+++ js/modules/export-lifestyle.js
@@ -3,7 +3,20 @@ function initModule() {
 }
 
 function downloadLifestyle() {
+  const rows = [
+    ['Position 1', '17'],
+    ['Position 2', '29'],
+  ]
 
+  let csvContent = "data:text/csv;charset=utf-8,";
+
+  rows.forEach(function (rowArray) {
+    let row = rowArray.join(",");
+    csvContent += row + "\r\n";
+  });
+
+  var encodedUri = encodeURI(csvContent);
+  window.open(encodedUri);
 }
 
 export {initModule}
