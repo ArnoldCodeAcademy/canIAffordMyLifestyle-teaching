9d10087cde4dae10e7dfd42fd0e361527220db82
arnoldabraham <info@arnoldcode.com>
2023-01-12 20:56:44 +0100

    feat(export): final step to export all positions

--- js/modules/export-lifestyle.js
+++ js/modules/export-lifestyle.js
@@ -7,7 +7,7 @@ function initModule() {
 function downloadLifestyle() {
   const expensesAndPositions = linkedInputs.filter(x => !x.id.includes('tax'));
 
-
+  const rows = convertForExport(expensesAndPositions);
 
   let csvContent = "data:text/csv;charset=utf-8,";
 
@@ -18,6 +18,25 @@ function downloadLifestyle() {
 
   var encodedUri = encodeURI(csvContent);
   window.open(encodedUri);
+}
+
+function convertForExport(expensesAndPositions) {
+  const rawArray = expensesAndPositions.map(x => x.value);
+  return convertArrayIntoPairOfTwos(rawArray)
+}
+
+function convertArrayIntoPairOfTwos(arr) {
+  // map through the array
+  return arr.map((value, index) => {
+    // check if the current index is even
+    if (index % 2 === 0) {
+      // if it is, return a subarray with the current value and the next value in the array
+      return [value, arr[index + 1]];
+    }
+    // if not, return null
+    return null;
+    // filter out any null values
+  }).filter(Boolean);
 }
 
 export {initModule}
